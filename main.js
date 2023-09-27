4db6fbfef773b5a7bd68da87d16ddf5611006f20
Federico Maeda <fedemaeda@gmail.com>
2023-09-27 19:21:33 -0300

    prueba 1

--- main.js
+++ main.js
@@ -1,36 +1,28 @@
-class Api {
-    constructor(instabot, followers, quantity) {
-      this.instabot = instabot;
-      this.followers = followers;
-      this.quantity = quantity;
-    }
-  
-    async start() {
-      let index = 0;
-      while (index < this.followers.length) {
-        for (let i = 0; i < this.quantity; i++) {
-          if (index < this.followers.length) {
-            await this.comment(index);
-            index++;
-          }
-        }
-  
-        if (index % 20 === 0) {
-          console.log("Haciendo una pausa de 5 minutos...");
-          await sleep(300000);
-        }
-      }
-    }
-  
-    async comment(index) {
-      const friend = this.followers[index];
-      const comment = "@" + friend.username;
-  
-      if (index < this.quantity - 1) {
-        comment += ", ";
-      }
-  
-      await this.instabot.comment(this.url, comment);
-    }
-  }
-  
+const clientId = "857862365696803";
+const clientSecret = "7b9e7180931e9fe0fb6f90a7a91ae094";
+
+async function etiquetarAmigos(url, cantidad, pausa) {
+  // Obtener el nombre de usuario y la contraseña
+  const username = "fedemaeda@gmail.com";
+  const password = "Fedemushi20";
+
+  // Instanciar la API
+  const instabot = new Instabot({
+    clientId,
+    clientSecret,
+    username,
+    password,
+  });
+
+  // Obtener la lista de seguidores
+  const followers = await instabot.getFollowers();
+
+  // Crear la API
+  const api = new Api(instabot, followers, cantidad);
+
+  // Iniciar la API
+  await api.start(pausa);
+}
+
+// Ejemplo de uso
+etiquetarAmigos("https://www.instagram.com/p/123456789/", 2, 300000);
